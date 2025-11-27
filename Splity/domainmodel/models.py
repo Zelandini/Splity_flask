a91c2c7667eabd3be82952e0e95592d300b6b466
zbg <33819974+Zelandini@users.noreply.github.com>
2025-11-27 16:19:10 +1300

    created models: for User, Bill, and BillParticipant

--- Splity/domainmodel/models.py
+++ Splity/domainmodel/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 class User:
     def __init__(self, user_id: int, name: str, username: str, email: str, password: str = None):
@@ -6,29 +7,64 @@ class User:
         self.__username = username
         self.__email = email
         self.__password = password
-        self.__bills = []
 
     @property
-    def id(self, user_id: int):
-        if user_id is not None and (type(user_id) is not int or user_id < 0):
-            raise ValueError("User ID must be a positive integer or None")
-        self.__id = user_id
+    def id(self):
+        return self.__id
 
     @property
-    def name(self, name: str):
-        self.__name = name
+    def name(self):
+        return self.__name
 
     @property
-    def username(self, username: str):
-        self.__username = username
+    def username(self):
+        return self.__username
 
     @property
-    def email(self, email: str):
-        self.__email = email
+    def email(self):
+        return self.__email
 
     @property
-    def password(self, password: str):
-        self.__password = password
+    def password(self):
+        return self.__password
 
     def __repr__(self):
         return f"<User {self.id}: {self.username}>"
+
+
+
+class Bill:
+    def __init__(self, bill_id: int, user_id: int, description: str, created_date: datetime):
+        self.__bill_id = bill_id
+        self.__user_id = user_id
+        self.__description = description
+        self.__data = created_date if created_date else datetime.now()
+
+    @property
+    def id(self):
+        return self.__bill_id
+
+    @property
+    def user_id(self):
+        return self.__user_id
+
+    @property
+    def description(self):
+        return self.__description
+
+    @property
+    def date(self):
+        return self.__data
+
+    def __repr__(self):
+        return f"<Bill {self.id}: {self.user_id}: {self.description}>"
+
+
+
+class BillParticipant:
+    def __init__(self, bill_id: int, user_id: int, amount_owed: float, has_paid: bool):
+        self.__bill_id = bill_id
+        self.__user_id = user_id
+        self.__amount_owed = amount_owed
+        self.__has_paid = has_paid
+
