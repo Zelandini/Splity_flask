29de231fc4d84d25619ea99ced854be5f14741a0
zbg <33819974+Zelandini@users.noreply.github.com>
2025-12-23 23:26:23 +0900

    Create form layer to separate from the routing layer.

---
+++ Splity/forms/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm Password',
+                              validators=[DataRequired(),
+                                          EqualTo('password', message='Passwords must match.')])
+    submit = SubmitField('Register')
+
+
+class GroupCreationForm(FlaskForm):
+    name = StringField('Group Name', validators=[DataRequired(), Length(min=2, max=50)])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    currency = SelectField('Currency', choices=[], validators=[DataRequired()])
+    submit = SubmitField('Create Group')
