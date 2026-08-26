9a7a4c114c12d0ae7dc11f11010dbe7216edfd2e
zbg <33819974+Zelandini@users.noreply.github.com>
2026-08-26 15:16:05 +1200

    Configure Google authentication

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
-# /Splity_flask/config.py
-
 import os
-
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///splity.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY=os.environ.get("SECRET_KEY") or "development-only-change-before-deployment"
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL") or "sqlite:///splity.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    GOOGLE_CLIENT_ID=os.environ.get("GOOGLE_CLIENT_ID")
+    GOOGLE_CLIENT_SECRET=os.environ.get("GOOGLE_CLIENT_SECRET")
+    SESSION_COOKIE_HTTPONLY=True
+    SESSION_COOKIE_SAMESITE="Lax"
