401c4a8ea7cd12c3fed533e449f4273a73716884
zbg <33819974+Zelandini@users.noreply.github.com>
2026-08-26 15:00:45 +1200

    Implement database.py feature rules

--- Splity/adapters/database.py
+++ Splity/adapters/database.py
@@ -1,18 +1,9 @@
-# /Splity_flask/Splity/adapters/database.py
-
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 def init_db(app):
     db.init_app(app)
-
     with app.app_context():
-        # Import inside context to register models to this specific 'db' instance
-        from Splity.adapters.orm import UserORM, BillORM, BillParticipantORM, GroupORM
-        print(f"DEBUG: Database is at {app.instance_path}")
-        # Verification print
-        print(f">>> Tables in registry: {list(db.metadata.tables.keys())}")
-
+        from Splity.adapters.orm import UserORM, BillORM, BillParticipantORM, GroupORM, RepaymentORM
         db.create_all()
-        print(">>> Database initialized!")
